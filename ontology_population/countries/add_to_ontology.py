711266502f0bdb68aca7116c52cabe2f8d5947bc
Costin Busioc <busioccostin@gmail.com>
2020-04-02 23:51:11 +0300

    Add countries in ontology

---
+++ ontology_population/countries/add_to_ontology.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from rdflib import Graph
+from rdflib import URIRef, Namespace, Literal
+from rdflib.namespace import RDF, FOAF
+
+input_table = 'countries.csv'
+input_ontology = 'ontology.owl'
+output_ontology = 'new_ontology.owl'
+
+ontology_root = "http://purl.org/sport/ontology/"
+dbpedia_root = "http://dbpedia.org/ontology/"
+
+sports_ontology = Namespace(ontology_root)
+dbpedia = Namespace(dbpedia_root)
+
+g = Graph()
+
+g.parse(input_ontology, format="xml")
+
+df = pd.read_csv(input_table)
+for idx, row in df.iterrows():
+    country_name = row['Country']
+    country_URI = URIRef(ontology_root + country_name.replace(' ', ''))
+    
+    g.add((country_URI, RDF.type, dbpedia.Country))
+    g.add((country_URI, dbpedia.informationName, Literal(country_name)))
+
+g.serialize(destination=output_ontology, format="xml")
