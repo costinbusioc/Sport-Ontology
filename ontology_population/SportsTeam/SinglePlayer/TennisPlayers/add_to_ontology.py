0809148ad54e771a4b80fc20356bc6a7ad8ae63f
Costin Busioc <busioccostin@gmail.com>
2020-04-03 11:58:31 +0300

    Add tennis players as single player team

---
+++ ontology_population/SportsTeam/SinglePlayer/TennisPlayers/add_to_ontology.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+from rdflib import Graph
+from rdflib import URIRef, Namespace, Literal
+from rdflib.namespace import RDF, FOAF
+
+input_table = 'tennis_players.csv'
+input_ontology = 'ontology.owl'
+output_ontology = 'new_ontology.owl'
+
+ontology_root = "http://purl.org/sport/ontology/"
+dbpedia_root = "http://dbpedia.org/ontology/"
+
+sports_ontology = Namespace(ontology_root)
+dbpedia = Namespace(dbpedia_root)
+
+g = Graph()
+
+g.parse(input_ontology, format="xml")
+
+df = pd.read_csv(input_table)
+for idx, row in df.iterrows():
+    tennis_player_name = row['Name']
+    tennis_player_URI = URIRef(ontology_root + tennis_player_name.replace(' ', ''))
+    tennis_player_single_player_URI = URIRef(ontology_root + tennis_player_name.replace(' ', '') + 'SinglePlayerTeam')
+
+    g.add((tennis_player_single_player_URI, RDF.type, sports_ontology.SinglePlayer))
+    g.add((tennis_player_single_player_URI, dbpedia.playerInTeam, tennis_player_URI))
+
+g.serialize(destination=output_ontology, format="xml")
