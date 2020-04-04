b39104d6518d15d9a310230730405bb4b531a649
Costin Busioc <busioccostin@gmail.com>
2020-04-05 00:50:45 +0300

    Add ATP matches in ontology

---
+++ ontology_population/SportEvents/Match/Tennis/add_to_ontology.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+from rdflib import Graph
+from rdflib import URIRef, Namespace, Literal
+from rdflib.namespace import RDF, FOAF
+
+input_tables = ['tennis_matches.csv', 'tennis_matches1.csv', 'tennis_matches2.csv']
+input_ontology = 'ontology.owl'
+output_ontology = 'ontology.owl'
+
+ontology_root = "http://purl.org/sport/ontology/"
+dbpedia_root = "http://dbpedia.org/ontology/"
+
+sports_ontology = Namespace(ontology_root)
+dbpedia = Namespace(dbpedia_root)
+
+g = Graph()
+
+g.parse(input_ontology, format="xml")
+
+for input_table in input_tables:
+    df = pd.read_csv(input_table)
+    for idx, row in df.iterrows():
+        cal_year = row['Year']
+        tournament = row['Tournament']
+        city = row['City']
+        country = row['Country']
+        home_player = row['Home']
+        away_player = row['Away']
+        home_score = row['Home Score']
+        away_score = row['Away Score']
+
+        if country == 'Great Britain':
+            country = 'United Kingdom'
+        if country == 'United States':
+            country = 'United States of America'
+        if city == 'New York':
+            city = 'New York City'
+        if city == 'Washington':
+            city = 'Washington D.C.'
+
+        match_URI = URIRef(ontology_root + home_player.replace(' ' , '') + '-' + away_player.replace(' ', '') + tournament.replace(' ', '') + str(cal_year))
+        home_URI = URIRef(ontology_root + home_player.replace(' ', '') + 'SinglePlayerTeam')
+        away_URI = URIRef(ontology_root + away_player.replace(' ', '') + 'SinglePlayerTeam')
+        tournament_URI = URIRef(ontology_root + tournament.replace(' ' , '') + str(cal_year))
+        if country != 'Empty':
+            country_URI = URIRef(ontology_root + country.replace(' ', ''))
+        try:
+            city_URI = URIRef(ontology_root + city.replace(' ', ''))
+        except:
+            continue
+
+        g.add((match_URI, RDF.type, sports_ontology.Match))
+        g.add((match_URI, sports_ontology.hasMatchName, Literal(home_player + ' - ' + away_player)))
+        g.add((match_URI, sports_ontology.hasHomeScore, Literal(home_score)))
+        g.add((match_URI, sports_ontology.hasAwayScore, Literal(away_score)))
+        g.add((match_URI, sports_ontology.hasTournament, tournament_URI))
+        if country != 'Empty':
+            g.add((match_URI, sports_ontology.hasLocation, country_URI))
+        g.add((match_URI, sports_ontology.hasLocation, city_URI))
+        g.add((tournament_URI, sports_ontology.hasSportType, sports_ontology.Tennis))
+        g.add((match_URI, sports_ontology.hasHomeTeam, home_URI))
+        g.add((match_URI, sports_ontology.hasAwayTeam, away_URI))
+
+g.serialize(destination=output_ontology, format="xml")
--- ontology_population/SportEvents/Match/Tennis/crawl.py
+++ ontology_population/SportEvents/Match/Tennis/crawl.py
@@ -14,7 +14,7 @@ sys.path.append('../../../helpers')
 from helpers import write_csv
 
 root_url = "https://www.atptour.com/"
-#input_tables = ["tennis_tournaments.csv", "new_tennis_tournaments.csv"]
+input_tables = ["tennis_tournaments.csv", "new_tennis_tournaments.csv"]
 
 paranthesis = re.compile(r'\([^)]*\)')
 
@@ -27,82 +27,80 @@ years = []
 cities = []
 countries = []
 
-input_table = "new_tennis_tournaments.csv"
-df = pd.read_csv(input_table)
+for input_table in input_tables:
+    df = pd.read_csv(input_table)
+    for idx, row in df.iterrows():
+        year = row['Year']
+        tournament = row['Tournament']
+        startDate = row['Start Date']
+        city = row['City']
+        country = row['Country']
+        results_url = row['URL']
 
-curr_year = 2005
-for idx, row in df.iterrows():
-    year = row['Year']
-    tournament = row['Tournament']
-    startDate = row['Start Date']
-    city = row['City']
-    country = row['Country']
-    results_url = row['URL']
+        if year < 2014:
+            continue
 
-    if year < 2014:
-        continue
+        if year == 2014 and tournament == 'Memphis':
+            continue
 
-    if year == 2014 and tournament == 'Memphis':
-        continue
+        year, month, day = startDate.split('.')
+        startDate = datetime(int(year), int(month), int(day))
 
-    year, month, day = startDate.split('.')
-    startDate = datetime(int(year), int(month), int(day))
+        current_url = f"{root_url}{results_url}"
+        page = requests.get(current_url)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    current_url = f"{root_url}{results_url}"
-    page = requests.get(current_url)
-    soup = BeautifulSoup(page.content, "html.parser")
+        print(tournament)
+        print(year)
 
-    print(tournament)
-    print(year)
+        table = soup.find('table', class_='day-table')
+        sections = table.find_all('tbody')
+        for i, section in enumerate(sections):
+            matches_section = section.find_all('tr')
+            for j, match_section in enumerate(matches_section):
+                players = match_section.find_all('td', class_='day-table-name')
+                try:
+                    player1 = players[0].find('a').text.strip()
+                except:
+                    continue
 
-    table = soup.find('table', class_='day-table')
-    sections = table.find_all('tbody')
-    for i, section in enumerate(sections):
-        matches_section = section.find_all('tr')
-        for j, match_section in enumerate(matches_section):
-            players = match_section.find_all('td', class_='day-table-name')
-            try:
-                player1 = players[0].find('a').text.strip()
-            except:
-                continue
+                try:
+                    player2 = players[1].find('a').text.strip()
+                except:
+                    continue
 
-            try:
-                player2 = players[1].find('a').text.strip()
-            except:
-                continue
+                try:
+                    score = match_section.find('td', class_='day-table-score')
+                except:
+                    continue
+                try:
+                    score = score.find('a').text.strip()
+                except:
+                    continue
+                score = score.split('\n')
 
-            try:
-                score = match_section.find('td', class_='day-table-score')
-            except:
-                continue
-            try:
-                score = score.find('a').text.strip()
-            except:
-                continue
-            score = score.split('\n')
+                homescore = 0
+                awayscore = 0
+                if len(score) == 2:
+                    homescore = 2
+                elif len(score) == 3:
+                    homescore = 2
+                    awayscore = 1
+                elif len(score) == 4:
+                    homescore = 3
+                    awayscore = 1
+                elif len(score) == 5:
+                    homescore = 3
+                    awayscore = 2
 
-            homescore = 0
-            awayscore = 0
-            if len(score) == 2:
-                homescore = 2
-            elif len(score) == 3:
-                homescore = 2
-                awayscore = 1
-            elif len(score) == 4:
-                homescore = 3
-                awayscore = 1
-            elif len(score) == 5:
-                homescore = 3
-                awayscore = 2
-
-            home_names.append(player1)
-            away_names.append(player2)
-            home_scores.append(homescore)
-            away_scores.append(awayscore)
-            tournaments.append(tournament)
-            years.append(year)
-            cities.append(city)
-            countries.append(country)
+                home_names.append(player1)
+                away_names.append(player2)
+                home_scores.append(homescore)
+                away_scores.append(awayscore)
+                tournaments.append(tournament)
+                years.append(year)
+                cities.append(city)
+                countries.append(country)
 
 cols_name = ['Home', 'Away', 'Home Score', 'Away Score', 'Tournament', 'Year', 'City', 'Country']
 cols = [home_names, away_names, home_scores, away_scores, tournaments, years, cities, countries]
